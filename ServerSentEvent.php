91df0b74f69d0e9c94d840adf8e79ef615fcf8f6
Ordinatueur <ordinatueur@localhost.localdomain>
2013-10-31 15:59:54 -0400

    Add some commentary

--- ServerSentEvent.php
+++ ServerSentEvent.php
@@ -1,19 +1,30 @@
 <?php
 /**
 *	@author 	PredaEngine (David LBD)
+*	@copyright	LBD Conception
+*	@since 		01 Nov 2013
 *	@version 	1.0.0
+*	@link 		https://github.com/PredaEngine/server-sent-event
 */
 class ServerSentEvent
 {
 	/**
-    * @var ServerSentEvent
+    *	@var 	ServerSentEvent
     */
 	private static $_instance;
 
+	// Disabled in order to user Singleton Pattern
 	private function __construct()
-	{}
+	{
 
-	public function process($callback,$interval)
+	}
+
+	/**
+	*	@param callable $callback Will be executed at a regular interval
+	*	@param int 		$interval Interval between execution
+	*	@return void
+	*/
+	public function process(callable $callback,$interval)
 	{
 		if(!is_int($interval) or !is_callable($callback))
 			throw new Exception("Error during process", 1);
@@ -23,9 +34,15 @@ class ServerSentEvent
 			sleep($interval);
 		}	
 	}
-
+	/**
+	*	@param array 	$message Contains différents informations that will be send to client
+	*	@return void
+	*/
 	public function send(array $message)
 	{
+		if (!is_array($message))
+			throw new Exception("$message must be an array", 1);
+			
 		if(isset($message['event'])){
 			echo "event: ". $message['event'] ."\n";
 		}
